8e4ca09e94f762c8f03535e94471e69b29bb0979
Amubasshir <themubasshir07@gmail.com>
2021-10-14 08:19:27 +0600

    environment variable added

--- src/Pages/Login/Firebase/Login/Login.js
+++ src/Pages/Login/Firebase/Login/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
+    <div className="mt-5">
       <h2>Please login</h2>
       <button className="btn btn-warning">Google Sign In</button>
     </div>
